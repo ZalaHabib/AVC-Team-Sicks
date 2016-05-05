913c1222f0db33baca76c2c5dd0817c7859cad59
MyVerySpecialSuperProfessionalUsername <hopkincame@myvuw.ac.nz>
2016-05-05 15:43:57 +1200

    Update Straight_Line.c

--- Straight_Line.c
+++ Straight_Line.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 
-extern "C" int InitHardware();
+extern "C" int init(int d_lev);
 extern "C" int Sleep(int sec, int usec);
 extern "C" int SetMotor(int motor, int dir, int speed);
 
 int main(int dir, int speed, int time){
 //Ensures RPi is operational
-InitHardware();
+init(0);
 //Set motor direction and speed
 SetMotor(1,dir,speed);
 SetMotor(2,dir,speed);
