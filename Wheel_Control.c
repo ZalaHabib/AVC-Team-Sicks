1a75a19ba08272b9b9753e313631b7f79a6305b6
MyVerySpecialSuperProfessionalUsername <hopkincame@myvuw.ac.nz>
2016-05-05 15:45:00 +1200

    Update Wheel_Control.c

--- Wheel_Control.c
+++ Wheel_Control.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 
-extern "C" int InitHardware();
+extern "C" int init(int d_lev);
 extern "C" int Sleep(int sec, int usec);
 extern "C" int SetMotor(int motor, int dir, int speed);
 
 int main(int dir, int speed, int time, int wheel){
 //Ensures RPi is operational
-InitHardware();
+init(0);
 //Set motor direction and speed
 SetMotor(wheel,dir,speed);
 //Set length of times that motors will be active
