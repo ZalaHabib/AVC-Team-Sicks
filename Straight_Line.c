259256f28ddbe3326838b362a6429d542533c737
MyVerySpecialSuperProfessionalUsername <hopkincame@myvuw.ac.nz>
2016-05-05 15:33:23 +1200

    Added files via upload
    Added uncompiled c code for moving robot in straight line

---
+++ Straight_Line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+
+extern "C" int InitHardware();
+extern "C" int Sleep(int sec, int usec);
+extern "C" int SetMotor(int motor, int dir, int speed);
+
+int main(int dir, int speed, int time){
+//Ensures RPi is operational
+InitHardware();
+//Set motor direction and speed
+SetMotor(1,dir,speed);
+SetMotor(2,dir,speed);
+//Set length of times that motors will be active
+Sleep(time,0);
+//Stop Motors
+SetMotor(1,1,0);
+SetMotor(2,1,0);
+return 0;}
+
