c62ad95866c1704a4ed6fc14b443026cac437e21
MyVerySpecialSuperProfessionalUsername <hopkincame@myvuw.ac.nz>
2016-05-10 12:55:39 +1200

    Update and rename Straight_Line.c to Straight_Line.cpp

---
+++ Straight_Line.cpp
@@ -0,0 +1,20 @@
+# include <stdio.h> 
+# include <time.h> 
+
+extern "C" int init(int d_lev); 
+extern "C" int Sleep(int sec, int usec); 
+extern "C" int set_motor(int motor , int speed ); 
+
+int main(){
+//Ensures RPi is operational
+init(1);
+//Set motor direction and speed
+set_motor(1, 200);
+set_motor(2, 200);
+//Set length of times that motors will be active
+Sleep(5,0);
+//Stop Motors
+set_motor(1, 0);
+set_motor(2, 0);
+return 0;}
+
