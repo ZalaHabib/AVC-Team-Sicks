f3fa460ed6d0a56bf3f0a42abdbbc5bd02080985
MyVerySpecialSuperProfessionalUsername <hopkincame@myvuw.ac.nz>
2016-05-21 18:49:21 +1200

    Update turn.cpp

--- turn.cpp
+++ turn.cpp
@@ -1,27 +1,26 @@
 int turn(int error){
-  //Left wheel turns backwards
   int left_wheel=0;
   int right_wheel=0;
   if(white<1){
     if(prev_error>0)
       //If path was lost and was last to the left, turn left sharply
-      left_wheel=200;
-      right_wheel=200;
+      left_wheel=-50;
+      right_wheel=50;
     }
     else{
       //If path was lost and was last to right, turn right sharply
-      left_wheel=-200;
-      right_wheel=-200;
+      left_wheel=50;
+      right_wheel=-50;
     }
   }
   else{
     if(error>0){
-      left_wheel=((-error/160)*120)-40;
-      right_wheel=((-error/160)*350)+40;
+      left_wheel=((error/160)*50)+50;
+      right_wheel=((-error/160)*300)+50;
     }
     else{
-      left_wheel=((-error/160)*350)-40;
-      right_wheel=((-error/160)*120)+40;
+      left_wheel=((error/160)*300)+50;
+      right_wheel=((-error/160)*50)+50;
     }
   set_motor(1,left_wheel);
   set_motor(2,right_wheel);
